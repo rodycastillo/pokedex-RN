78951b00e4c095c3b79c6e40b1265cbaa0a9416a
Carlito <rody.castillo.tacuri@gmail.com>
2021-12-04 19:45:53 -0500

    feat: add new content and hook

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { usePokemonPaginated } from '../hooks/usePokemonPage'
+import { styles } from '../theme/appTheme'
+
+export const HomeScreen = () => {
+
+    const {top} = useSafeAreaInsets();
+    const { simplePokemonList } = usePokemonPaginated();
+    console.log(simplePokemonList);
+    
+
+    return (
+        <>
+            <Image 
+                source={require('../assets/pokebola.png')}
+                style={ styles.pokebolaBG }
+            />
+            <Text style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                top: top + 20
+            }}>Pokedex</Text>
+        </>
+    )
+}
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    globalMargin: {
+        marginHorizontal: 20
+    },
+    pokebolaBG: {
+        position: 'absolute',
+        top:-100,
+        width: 300,
+        height: 300,
+        right: -100,
+        opacity: 0.2
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: 'bold'
+    }
+})
